aad7155648fec05a7a482a802f62ceeaa8798c21
Rayan Khales <6241246@edu.vaniercollege.qc.ca>
2025-10-27 10:40:29 -0400

    Finished Lab

--- src/lab02_rayankhales/Lab02_RayanKhales.java
+++ src/lab02_rayankhales/Lab02_RayanKhales.java
@@ -13,7 +13,12 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.image.*; 
 import java.util.Random;
+import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 
 /**
  *
@@ -29,33 +34,92 @@ public class Lab02_RayanKhales extends Application{
         launch(args);
     }
 
+    private int current = 0;
+    private Boolean isRunning = false;
+    private double time = 2000;
+    
     @Override
     public void start(Stage primarystage) {
         BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 250, 300);
+        Scene scene = new Scene(root, 250, 500);
         primarystage.setTitle("Java Games");
         primarystage.show();
         VBox middle = new VBox();
-        Label top = new Label("Random Game");
-        Label bottom = new Label("Waiting");
+        
+        Label top = new Label("Game Cycle");
+        BorderPane.setAlignment(top, Pos.BOTTOM_CENTER);
+        
+        Button playPause = new Button("Play");
+        Button speedUp = new Button("Speed Up");
+        Button speedDown = new Button("Speed Down");
+        HBox speed = new HBox(10, speedUp, speedDown);
+        speed.setAlignment(Pos.TOP_CENTER);
+        VBox buttons = new VBox(10, playPause, speed);
+        buttons.setAlignment(Pos.TOP_CENTER);
+        root.setBottom(buttons);
+        
         Label lblImage = new Label();
         root.setTop(top);
-        root.setBottom(bottom);
+        
         middle.getChildren().add(lblImage);
         middle.setAlignment(Pos.CENTER);
-        BorderPane.setAlignment(top, Pos.CENTER);
-        BorderPane.setAlignment(middle, Pos.CENTER);
-        BorderPane.setAlignment(bottom, Pos.CENTER);
         root.setCenter(middle);
         
         String path = "file:src/images/";
-        Random random = new Random();
-        int randomNumber = random.nextInt(120 - 101 + 1) + 101;
-        Image image = new Image(path + randomNumber + ".jpg");
-        lblImage.setGraphic(new ImageView(image));
+        Image[] images = new Image[20];
+        for (int i = 0; i < 20; i++) {
+            int imageNumber = 101 + i;
+            images[i] = new Image(path + imageNumber + ".jpg");
+        }
+        lblImage.setGraphic(new ImageView(images[0]));
+        
+        FadeTransition fadeIn = new FadeTransition(new Duration(time / 2), lblImage);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+        
+        FadeTransition fadeOut = new FadeTransition(new Duration(time / 2), lblImage);
+        fadeOut.setFromValue(1);
+        fadeOut.setToValue(0);
+        
+        fadeOut.setOnFinished(e -> {
+            current = (current + 1) % images.length;
+            lblImage.setGraphic(new ImageView(images[current]));
+            fadeIn.play();
+        });
+        
+        fadeIn.setOnFinished(e -> {
+            if(isRunning) {
+                fadeOut.setDelay(new Duration(time / 2));
+                fadeOut.play();
+            }
+        });
+        
+        playPause.setOnAction(e -> {
+            if (isRunning) {
+                isRunning = false;
+                fadeOut.pause();
+                fadeIn.pause();
+                playPause.setText("Play");
+            } else {
+                isRunning = true;
+                fadeOut.play();
+                playPause.setText("Pause");
+            }
+        });
+        
+        speedUp.setOnAction(e -> {
+            time = Math.max(500, time - 500);
+            fadeOut.setDuration(new Duration(time));
+            fadeIn.setDuration(new Duration(time));
+        });
+        
+        speedDown.setOnAction(e -> {
+            time += 500;
+            fadeOut.setDuration(new Duration(time));
+            fadeIn.setDuration(new Duration(time));
+        });
          
         primarystage.setScene(scene);
         primarystage.show();  
-    }
-    
+    }  
 }
